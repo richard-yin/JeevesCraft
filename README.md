bd0d4a9ab59a773dcfc83ea06dc8d9caf0040f0f
Andrew <abujaki21@gmail.com>
2013-10-10 18:36:19 -0400

    v1.1 Added horse armor and saddles

--- README.md
+++ README.md
@@ -6,14 +6,17 @@ Perhaps when I feel some code is useful for a plugin, I will release it as such.
 Contact me if you want to see some features released as plugins.
 
 Current Version:
-1.0.0
+1.1
 
 Functions:
 ----------
-+ Added config file
++ Configuration file
 + Crafting Recipes:
 	+ Rotten Flesh -> Leather
+	+ Iron, Gold, and Diamond Horse Armor
+	+ Saddles!
 
 Known Issues:
 -------------
- - Crafting grid does not clear immediately when shift + Click crafting
+ - Crafting grid does not clear immediately when shift + Click crafting leather
+ - Armor and Saddles not yet tested.
--- src/com/github/abujaki21/jeevesCraft/JeevesCraft.java
+++ src/com/github/abujaki21/jeevesCraft/JeevesCraft.java
@@ -7,7 +7,7 @@
  * 															*
  * website: https://github.com/abujaki21/minestock			*
  * 															*
- * Version: 1.0.0											*
+ * Version: 1.1												*
  * 															*
  * This software is presented AS IS and without warranty	*
  * of any kind. I will not be held responsible for			*
@@ -24,16 +24,19 @@ import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JeevesCraft extends JavaPlugin{
 	private FileConfiguration config;
 	private Logger logger;
+	private Server server;
 	
 	@Override
 	public void onEnable(){
 		logger = this.getLogger();
+		server = this.getServer();
 		logger.info("OnEnable");
 		if(!(new File(getDataFolder().toString().concat("config.yml")).exists())){
 			logger.info("Setting up for the first time...");
@@ -57,16 +60,51 @@ public final class JeevesCraft extends JavaPlugin{
 	
 	//-------Recipes--------
 	private void enableRecipes(){
-		Server server = this.getServer();
 		
+		//Recipe to turn Rotten Flesh into Leather
 		if(config.getBoolean("Recipe.Leather.Enabled")){
 			int amt = config.getInt("Recipe.Leather.FleshForLeather");
-		
-			//Add Reciepe rotten Flesh ==> Leather 
 			ShapelessRecipe leather = new ShapelessRecipe(new ItemStack(Material.LEATHER,1));
 			leather = leather.addIngredient(amt, Material.ROTTEN_FLESH);
 			server.addRecipe(leather);
 			logger.info("Added recipe: " + amt + " ROTTEN_FLESH for 1 LEATHER");
+		}
+		
+		//Redcipes for Horse armor:
+		// _ _ X
+		// X X X     - Where X is Iron, Gold, or Diamond
+		// X _ X
+		if(config.getBoolean("Recipe.HorseArmor.Iron")){
+			ShapedRecipe HAIarmor = new ShapedRecipe(new ItemStack(Material.IRON_BARDING));
+			HAIarmor.shape("  I","III","I I");
+			HAIarmor.setIngredient('I',Material.IRON_INGOT);
+			server.addRecipe(HAIarmor);
+			logger.info("Added recipe: Horse Armor (Iron)");
+		}
+		if(config.getBoolean("Recipe.HorseArmor.Gold")){
+			ShapedRecipe HAGarmor = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING));
+			HAGarmor.shape("  G","GGG","G G");
+			HAGarmor.setIngredient('G', Material.GOLD_INGOT);
+			server.addRecipe(HAGarmor);
+			logger.info("Added recipe: Horse Armor (Gold)");
+		}
+		if(config.getBoolean("Recipe.HorseArmor.Diamond")){
+			ShapedRecipe HADarmor = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING));
+			HADarmor.shape("  D","DDD","D D");
+			HADarmor.setIngredient('D', Material.DIAMOND);
+			server.addRecipe(HADarmor);
+			logger.info("Added recipe: Horse Armor (Diamond)");
+		}
+		
+		//Recipe for Saddle
+		if(config.getBoolean("Recipe.Saddle")){
+			ShapedRecipe saddle = new ShapedRecipe(new ItemStack(Material.SADDLE));
+			saddle.shape("  u","LLL","LIL");
+			saddle.setIngredient('u', Material.LEASH);
+			saddle.setIngredient('L', Material.LEATHER);
+			saddle.setIngredient('I', Material.IRON_INGOT);
+			server.addRecipe(saddle);
+			logger.info("Added recipe: Saddle");
 		}
 	}
 }
