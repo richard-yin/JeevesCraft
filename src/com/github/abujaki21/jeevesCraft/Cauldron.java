1d80e9731f0196a5444157b059bb54907ea93b42
Andrew <abujaki21@gmail.com>
2013-10-17 00:14:05 -0400

    caul

---
+++ src/com/github/abujaki21/jeevesCraft/Cauldron.java
@@ -0,0 +1,8 @@
+package com.github.abujaki21.jeevesCraft;
+
+public class Cauldron extends org.bukkit.material.Cauldron{
+	
+	//----Bring back old functionality of cauldrons
+	
+	//----Add new recipes -Mushroom soup
+}
