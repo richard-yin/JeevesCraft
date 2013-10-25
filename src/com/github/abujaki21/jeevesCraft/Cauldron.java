52a811234743ca81d665551a1df7db6598ecf817
Andrew <abujaki21@gmail.com>
2013-10-24 23:25:34 -0400

    Tweaked configuration
    moved from load/save config to load and save yaml configurations

--- src/com/github/abujaki21/jeevesCraft/Cauldron.java
+++
@@ -1,8 +0,0 @@
-package com.github.abujaki21.jeevesCraft;
-
-public class Cauldron extends org.bukkit.material.Cauldron{
-	
-	//----Bring back old functionality of cauldrons
-	
-	//----Add new recipes -Mushroom soup
-}
