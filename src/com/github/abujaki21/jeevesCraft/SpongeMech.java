e9ccb8b9fe9ebfe4c68ccfa889324082d9079238
Andrew <abujaki21@gmail.com>
2013-12-20 01:03:11 -0500

    Dev 35413-0103 Code review, Sponge tweak, branch clean

--- src/com/github/abujaki21/jeevesCraft/SpongeMech.java
+++ src/com/github/abujaki21/jeevesCraft/SpongeMech.java
@@ -16,7 +16,7 @@ public class SpongeMech implements Listener{
 		//Range = (Diameter - 1) / 2
 		//Eg. 5x5x5 block: range = (5-1)/2 = 4/2 = 2.
 
-		range = 2;
+		range = 2; 
 
 		//TODO: Make configurable if there's a demand for it
 		//range = jeeves.getConfig().getInt("Sponge.Range");
@@ -31,9 +31,9 @@ public class SpongeMech implements Listener{
 		int locY = event.getBlock().getY();
 		int locZ = event.getBlock().getZ();
 		World world = event.getBlock().getWorld();
-		for(int i = locX; i <= (locX + (2*range)); i++){ //Loop x
-			for(int j = locY; j <= (locY + (2*range)); j++){ //Loop y
-				for(int k = locZ; k <= (locZ + (2*range)); k++){ //Loop z
+		for(int i = locX - (2*range); i <= (locX + (2*range)); i++){ //Loop x
+			for(int j = locY - (2*range); j <= (locY + (2*range)); j++){ //Loop y
+				for(int k = locZ - (2*range); k <= (locZ + (2*range)); k++){ //Loop z
 					if (world.getBlockAt(i, j, k).getType() == Material.SPONGE){
 						event.setCancelled(true);
 						return;
@@ -42,33 +42,31 @@ public class SpongeMech implements Listener{
 			}
 		}
 	}
+	
+	@EventHandler
+	public void onSpongePlace(BlockPlaceEvent event){
+		//If a sponge block is placed
+		if(event.getBlockPlaced().getType() == Material.SPONGE){
+			//Get x y z values of the sponge
+			int locX = event.getBlockPlaced().getX() - range;
+			int locY = event.getBlockPlaced().getY() - range;
+			int locZ = event.getBlockPlaced().getZ() - range;
+			World world = event.getBlockPlaced().getWorld();;
+			Block curr;
 
-
-
-@EventHandler
-public void onSpongePlace(BlockPlaceEvent event){
-	//If a sponge block is placed
-	if(event.getBlockPlaced().getType() == Material.SPONGE){
-		//Get x y z values of the sponge
-		int locX = event.getBlockPlaced().getX() - range;
-		int locY = event.getBlockPlaced().getY() - range;
-		int locZ = event.getBlockPlaced().getZ() - range;
-		World world = event.getBlockPlaced().getWorld();;
-		Block curr;
-
-		//Loop through xyz and replace all water with air
-		for(int i = locX; i <= (locX + (2*range)); i++){ //Loop x
-			for(int j = locY; j <= (locY + (2*range)); j++){ //Loop y
-				for(int k = locZ; k <= (locZ + (2*range)); k++){ //Loop z
-					curr = world.getBlockAt(i, j, k); //X, Y, Z
-					if((curr.getType() == Material.WATER) ||
-							(curr.getType() == Material.STATIONARY_WATER) ||
-							(curr.getType() == Material.AIR)){
-						curr.setType(Material.AIR);
+			//Loop through xyz and replace all water with air
+			for(int i = locX; i <= (locX + (2*range)); i++){ //Loop x
+				for(int j = locY; j <= (locY + (2*range)); j++){ //Loop y
+					for(int k = locZ; k <= (locZ + (2*range)); k++){ //Loop z
+						curr = world.getBlockAt(i, j, k); //X, Y, Z
+						if((curr.getType() == Material.WATER) ||
+								(curr.getType() == Material.STATIONARY_WATER) ||
+								(curr.getType() == Material.AIR)){
+							curr.setType(Material.AIR);
+						}
 					}
 				}
 			}
 		}
 	}
-}
 }
